66ee200579c37011a88c2725a9a5726b112c312d
duyle <duylk@tenxtenx.com>
2024-12-03 11:54:36 +0700

    update

--- script/setup.sh
+++ script/setup.sh
@@ -96,6 +96,11 @@ EOF
 
   echo br_netfilter | tee /etc/modules-load.d/kubernetes.conf
 
+  if [ -f /etc/apt/keyrings/kubernetes-apt-keyring.gpg ]
+  then
+    rm -rf "/etc/apt/keyrings/kubernetes-apt-keyring.gpg"
+  fi
+
   install -m 0755 -d /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   chmod a+r /etc/apt/keyrings/docker.asc
